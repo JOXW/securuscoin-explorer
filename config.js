ca2f4ddd01410ac75dfefb669063ca7b60548a99
JOXW <>
2019-03-14 16:19:56 +0100

    init

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
-var api = 'http://127.0.0.1:11898';
+var api = 'localhost:14115';
 var donationAddress = "";
-var blockTargetInterval = 30; // enter the block interval in seconds
-var coinUnits = 100;  // enter in the amount of atomic units in 1 coin, eg. 100 shells = 1 trtl
-var totalSupply =  100000000000000; // enter the total supply in atomic units
-var symbol = 'trtl'; // enter the coin's ticker
+var blockTargetInterval = 60; // enter the block interval in seconds
+var coinUnits = 1000;  // enter in the amount of atomic units in 1 coin, eg. 100 shells = 1 trtl
+var totalSupply =  33000000000; // enter the total supply in atomic units
+var symbol = 'SCR'; // enter the coin's ticker
 var refreshDelay = 30000;
 
 // pools stats by MainCoins
